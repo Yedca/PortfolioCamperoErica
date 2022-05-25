493727d35408f84dc6d9cf8a6bf5dba1ccc3d10d
Yedca <96070677+Yedca@users.noreply.github.com>
2022-05-25 00:19:46 -0300

    subida de imagenes

--- ProyectoCampero/src/app/app.module.ts
+++ ProyectoCampero/src/app/app.module.ts
@@ -2,10 +2,16 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { LogoComponent } from './components/logo/logo.component';
+import { SocialComponent } from './components/social/social.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    LogoComponent,
+    SocialComponent
   ],
   imports: [
     BrowserModule
