ec16f8d767d6f19289c2c04cdb000be066c6f045
Yedca <96070677+Yedca@users.noreply.github.com>
2022-05-25 18:45:41 -0300

    creacion de componente acerca-de

--- ProyectoCampero/src/app/app.module.ts
+++ ProyectoCampero/src/app/app.module.ts
@@ -6,6 +6,7 @@ import { HeaderComponent } from './components/header/header.component';
 import { LogoComponent } from './components/logo/logo.component';
 import { SocialComponent } from './components/social/social.component';
 import { BannerComponent } from './components/banner/banner.component';
+import { AcercaDeComponent } from './components/acerca-de/acerca-de.component';
 
 @NgModule({
   declarations: [
@@ -13,7 +14,8 @@ import { BannerComponent } from './components/banner/banner.component';
     HeaderComponent,
     LogoComponent,
     SocialComponent,
-    BannerComponent
+    BannerComponent,
+    AcercaDeComponent
   ],
   imports: [
     BrowserModule
