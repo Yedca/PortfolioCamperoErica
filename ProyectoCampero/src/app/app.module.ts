7987d95c842e76796f0a2722bfe4ce0209be8bc8
Yedca <96070677+Yedca@users.noreply.github.com>
2022-05-27 01:35:43 -0300

    componente hys y ng-circle

--- ProyectoCampero/src/app/app.module.ts
+++ ProyectoCampero/src/app/app.module.ts
@@ -9,6 +9,9 @@ import { BannerComponent } from './components/banner/banner.component';
 import { AcercaDeComponent } from './components/acerca-de/acerca-de.component';
 import { ExperienciaComponent } from './components/experiencia/experiencia.component';
 import { EducacionComponent } from './components/educacion/educacion.component';
+import { NgCircleProgressModule } from 'ng-circle-progress';
+import { HyskillsComponent } from './components/hyskills/hyskills.component';
+
 
 @NgModule({
   declarations: [
@@ -19,10 +22,12 @@ import { EducacionComponent } from './components/educacion/educacion.component';
     BannerComponent,
     AcercaDeComponent,
     ExperienciaComponent,
-    EducacionComponent
+    EducacionComponent,
+    HyskillsComponent,
   ],
   imports: [
     BrowserModule
+    NgCircleProgressModule.forRoot ({})
   ],
   providers: [],
   bootstrap: [AppComponent]
