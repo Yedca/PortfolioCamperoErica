8eaa6846dc04c62f6a3ffa50c79a198830820b0a
Yedca <96070677+Yedca@users.noreply.github.com>
2022-05-28 20:56:39 -0300

    problemas para compilar y creacion del componente/proyecto

--- ProyectoCampero/src/app/app.module.ts
+++ ProyectoCampero/src/app/app.module.ts
@@ -11,6 +11,7 @@ import { ExperienciaComponent } from './components/experiencia/experiencia.compo
 import { EducacionComponent } from './components/educacion/educacion.component';
 import { NgCircleProgressModule } from 'ng-circle-progress';
 import { HyskillsComponent } from './components/hyskills/hyskills.component';
+import { ProyectoComponent } from './components/proyecto/proyecto.component';
 
 
 @NgModule({
@@ -24,6 +25,7 @@ import { HyskillsComponent } from './components/hyskills/hyskills.component';
     ExperienciaComponent,
     EducacionComponent,
     HyskillsComponent,
+    ProyectoComponent,
   ],
   imports: [
     BrowserModule,
